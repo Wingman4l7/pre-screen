6cbcab443931ca38b39dae339333b41f92237ac0
Wingman4l7 <gouldfish@gmail.com>
2019-08-25 18:18:11 -0700

    Working version.

--- birthdayEmailer.py
+++ birthdayEmailer.py
@@ -1,5 +1,4 @@
-from redis import createClient
-from localLibs import sendBirthdayEmail
+import redis
 import asyncio
 import datetime
 import unittest
@@ -7,16 +6,19 @@ import unittest
 
 class TestBirthdayEmailer(unittest.TestCase):
   
-  today = datetime.date.today()
+  # def __init__(self, methodName='runTest'):
+  #   today = datetime.date.today()
+
+  # need to run a client to do tests...
 
   def test_isBirthday(self):
-    testUser = User(1, today)
+    testUser = User(1, datetime.date.today())
     self.assertTrue(testUser.isBirthday())
     testUser.birthday = today - datetime.timedelta(1)  # yesterday's date
     self.assertFalse(testUser.isBirthday())
 
   def test_hasSentThisYear(self):
-    testUser = User(2, today) 
+    testUser = User(2, datetime.date.today()) 
     key = "sent-" + str(2)
     testUser.redis.set(key, today.year) 
     self.assertTrue(testUser.hasSentThisYear(key))
@@ -24,7 +26,7 @@ class TestBirthdayEmailer(unittest.TestCase):
     self.assertFalse()    
 
   def test_setSentStatus(self):  
-    testUser = User(3, today)
+    testUser = User(3, datetime.date.today())
     key = "sent-" + str(3)
     testUser.setSentStatus(key)
     self.assertEquals(testUser.redis.get(key), today.year)
@@ -32,63 +34,68 @@ class TestBirthdayEmailer(unittest.TestCase):
 
 class User:
 
-  def __init__(self, id, birthday):
-    self.id = id  # cleaner constructor
-    self.birthday = birthday
-    self.redis = redis.createClient()
-
+  def __init__(self, id, birthday, client):
+    self.id = id
+    self.birthday = datetime.datetime.strptime(birthday, '%Y-%m-%d')
+    self.redis = client
   
   # made return function more explicit, so it is more easily readable
   # using the full year as part of the conditional will result in false negatives!  Removed.
-  def isBirthday(): 
+  def isBirthday(self): 
     today = datetime.date.today()
-    if self.birthday.month == today.month and 
-       self.birthday.day == today.day:
-      return True
-    else:
-      return False
+    return self.birthday.month == today.month and self.birthday.day == today.day
 
-  async def celebrateBirthday():
-    key = "sent-" + str(self.id)  
-    hasSent = await self.hasSentThisYear(key)
+  async def celebrateBirthday(self, key):
+    hasSent = self.hasSentThisYear(key)
     if not hasSent:
-      await sendBirthdayEmail()
-    await self.setSentStatus(key)
-    # self.saveBirthday()  # not used
-
-  # DEPRECATED -- not used!
-  # def saveBirthday():  # more descriptive function name was needed
-  #   self.redis.hset('users', self.id, 'birthday', self.birthday.toISOString())
-  #   # would be better to have 'birthday' value be Date object, if supported by redis.
+      sendBirthdayEmail(key)
+    self.setSentStatus(key)
   
-  def hasSentThisYear(key):
-    sentYear = self.redis.get(key)
-    if sentYear == datetime.date.today().year:
-      return True
-    else:
-      return False
+  def hasSentThisYear(self, key):
+    sentYear = self.redis.hget(key, 'sentYear')
+    return sentYear == str(datetime.date.today().year)
 
-  def setSentStatus(key):
+  def setSentStatus(self, key):
     currentYear = datetime.date.today().year
-    self.redis.set(key, currentYear); 
+    self.redis.hset(key, 'sentYear', currentYear)
 
-  @staticmethod
-  async def findUser(id):  # needed a more consistent/explanatory function name
-    user = await new User().redis.hget('users', id)
-    return new User(user.id, new Date(user.birthday))
-
-  def iterateOverUsers():
-    client = createClient()
-    IdSet = await client.get('age-app:user-ids')
-    for i in range(IdSet):
-      user = await User.findUser(i)
-      if user.isBirthday():
-        await user.celebrateBirthday()
+  async def findUser(client, key):
+    user = client.hgetall(key)
+    return User(user["id"], user["birthday"], client)    
 
 
-  async def main():
-    iterateOverUsers()
+async def iterateOverUsers(client):
+  # BEST PRACTICE: ( installed version of redis doesn't support scan_iter() )
+  # for key in client.scan_iter("user-*"): # use built-in iterating function
+  keys = client.keys('user-*')
+  for key in keys:
+    user = await User.findUser(client, key)
+    if user.isBirthday():
+      await user.celebrateBirthday(key)
+
+# FOR TESTING
+def addTestEntries(client):
+  client.hmset('user-1', {'id' : '1', 'birthday' : '2000-12-30'})
+  client.hmset('user-2', {'id' : '2', 'birthday' : '2000-08-25'})
+  client.hmset('user-3', {'id' : '3', 'birthday' : '2000-08-25', 'sentYear' : '2019'})
+
+# STUB function to get code to run completely:
+def sendBirthdayEmail(key):
+  print("It's " + key + " 's birthday!")
+
+def main():
+
+  # arguments should be set / accessed via config file:
+  client = redis.StrictRedis(
+  host="localhost",
+  port=6379, 
+  charset ="utf-8",
+  decode_responses=True)
+
+  addTestEntries(client)
+  asyncio.run(iterateOverUsers(client))
 
 # process main method call
 if __name__ == '__main__':
-    main()
+  # unittest.main()
+  main()
